524fc55738a28ea0e38ae798984f43dea460e93d
Henning Leutz <leutz@pcsg.de>
2023-09-04 13:22:57 +0200

    feat: new event shippingOrderProcessSteps

--- src/QUI/ERP/Shipping/OrderProcessProvider.php
+++ src/QUI/ERP/Shipping/OrderProcessProvider.php
@@ -6,12 +6,15 @@
 
 namespace QUI\ERP\Shipping;
 
+use QUI;
 use QUI\ERP\Order\AbstractOrder;
 use QUI\ERP\Order\AbstractOrderProcessProvider;
 use QUI\ERP\Order\Controls\AbstractOrderingStep;
 use QUI\ERP\Order\OrderProcess;
 use QUI\ERP\Order\Utils\OrderProcessSteps;
 use QUI\ERP\Shipping\Api\AbstractShippingEntry;
+
+use function is_bool;
 
 /**
  * Class OrderProcessProvider
@@ -44,6 +47,23 @@ class OrderProcessProvider extends AbstractOrderProcessProvider
         if ($Process->getOrder()) {
             $Order = $Process->getOrder();
             $orderId = $Order->getId();
+        }
+
+
+        try {
+            $result = QUI::getEvents()->fireEvent(
+                'shippingOrderProcessSteps',
+                [$this, $OrderProcessSteps, $Process, $Order]
+            );
+
+            if (!empty($result)) {
+                foreach ($result as $entry) {
+                    if (is_bool($entry) && $entry === false) {
+                        return;
+                    }
+                }
+            }
+        } catch (\Exception $exception) {
         }
 
         $OrderProcessSteps->append(
